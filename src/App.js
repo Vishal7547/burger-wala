5fe96bcbc192e4b69481839c10684833c6c05af4
Vishal kumar <vishalk80269@gmail.com>
2023-05-21 01:25:06 +0530

    navbar completed and uses framer motion and scss for css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./component/home/Home";
 import Header from "./component/layout/Header";
+import "./style/app.scss";
+import "./style/header.scss";
+
 function App() {
   return (
     <div className="App">

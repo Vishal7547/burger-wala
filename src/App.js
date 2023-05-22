e7afc71017daa2c8e73141a80d87c4f1d45d81fd
Vishal kumar <vishalk80269@gmail.com>
2023-05-22 22:44:05 +0530

    ui part is complete

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import ConfirmOrder from "./component/cart/ConfirmOrder.jsx";
 import PaymentSuccess from "./component/cart/PaymentSuccess.jsx";
 import Login from "./component/login/Login";
 import Profile from "./component/profile/Profile";
+import MyOrders from "./component/myOrder/MyOrders";
+import OrderDetails from "./component/myOrder/OrderDetails.jsx";
+import Dashboard from "./component/admin/Dashboard";
+import Users from "./component/admin/Users";
+import Orders from "./component/admin/Orders";
+import About from "./component/about/About";
 import "./style/app.scss";
 import "./style/header.scss";
 import "./style/home.scss";
@@ -22,6 +28,10 @@ import "./style/confirmOrder.scss";
 import "./style/PaymentSuccess.scss";
 import "./style/login.scss";
 import "./style/profile.scss";
+import "./style/table.scss";
+import "./style/orderDetails.scss";
+import "./style/dashboard.scss";
+import "./style/about.scss";
 
 function App() {
   return (
@@ -37,6 +47,12 @@ function App() {
           <Route path="/paymentsuccess" element={<PaymentSuccess />} />
           <Route path="/login" element={<Login />} />
           <Route path="/me" element={<Profile />} />
+          <Route path="/myorders" element={<MyOrders />} />
+          <Route path="/order/:id" element={<OrderDetails />} />
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/users" element={<Users />} />
+          <Route path="/admin/orders" element={<Orders />} />
+          <Route path="/about" element={<About />} />
         </Routes>
         <Footer />
       </Router>
